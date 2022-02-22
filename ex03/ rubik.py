f534aeae0ca13c8af9e396d5f472296a51620cc2
Chupsa-Polya <Chupsa-polkak@mail.ru>
2022-02-22 10:51:05 +0300

    Fix ex03

--- ex03/ rubik.py
+++ ex03/ rubik.py
@@ -1,6 +1,19 @@
+from turtle import colormode
+
+
 a = str(input())
 b = str(input())
 c = str(input())
-if a[1] == b[0] and b[0-2] == c[1]:
-    print("верно")
-else: print("не верно")
+color = a[1]
+flag = True
+for i in b:
+    if i != color:
+        flag = False
+        break
+if c[1]!=color:
+    flag = False
+
+if flag == True:
+    print ("верно")
+else: 
+    print ("не верно")
